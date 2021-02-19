8b99865de7e01a72cdd930c0454cab67e096468b
sriniwas14 <sriniwasx@gmail.com>
2021-02-19 10:23:14 +0530

    Implemented Socket Handshake and Sending Direct Messages

--- Backend/socket.js
+++ Backend/socket.js
@@ -1,10 +1,25 @@
 exports.chatSocket = (app) => {
     const io = require("socket.io")(app, { cors:true, origins: "*" })
+
+    // All the Active Connections Will Be Stored Here
+    let connections = {}
+
     io.sockets.on('connection', (socket) => {
         console.log("New User Connected!")
 
+        // Get User E-mail and create an entry with socketId in the connections object
+        socket.on('handshake', data => {
+            connections[data.email] = socket.id
+        });
+
+        // Send and Receive Messages 
         socket.on('send message', (msg) => {
-            io.emit('chat message', msg);
+            const socketId = connections[msg.recepientId] 
+            if (socketId) {
+                // Send Message to `recepient`
+                io.to(socketId).emit('chat message', msg)
+            }
+            // Implement Save to Database
         });
     })
 }
